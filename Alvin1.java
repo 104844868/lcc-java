293ea23ec3429441b1f8cf25b1f5dab1b2a7a8b9
104844868 <109008187+104844868@users.noreply.github.com>
2022-07-15 14:56:45 +0800

    lcc 作業一
    如何判斷數字是基數或是偶數

---
+++ Alvin1.java
@@ -0,0 +1,23 @@
+package student;
+
+public class Alvin1 {
+	public static void main(String[] args) {
+	
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+
+		System.out.print("請輸入數字");
+		
+		int age = scan.nextInt();
+			      
+		
+		if (age % 2 == 0) {
+			System.out.println("偶數");	
+			System.out.println("執行完畢");
+		}else {
+			System.out.println("基數");	
+				System.out.print("執行完畢");
+			
+		}
+	    }
+	
+	}
